164ee0e3b3dd44005d9ed4eddf4c757c74db30e6
PeachRapit1022 <ia.rapit22@gmail.com>
2022-10-26 10:57:13 +0900

    重複再試行完成

--- setPokemonID.js
+++ setPokemonID.js
@@ -1,5 +1,6 @@
 //pokeAPIから取得したタイプを保存するグローバル変数
 let trueType = [];
+//ひとつ前のIDを保存する変数
 let pastPokemonId = 0;
 
 window.addEventListener('load', function() {
@@ -11,32 +12,38 @@ window.addEventListener('load', function() {
 */
 function doGet() {
   trueType = []
-  //全国図鑑番号
-  let pokemonId = setPokemonId();
-  if (pokemonId == pastPokemonId) {
+
+  //ID選択モードを取得
+  const inputMode = document.getElementById('inputMode').elements['inputMode'].value;
+
+  //全国図鑑番号を取得
+  let pokemonId = setPokemonId(inputMode);
+
+  //手動入力時以外は重複再試行
+  if (inputMode!=='manual' && pokemonId === pastPokemonId) {
     console.log('重複再試行')
-    pokemonId = setPokemonId();
+    pokemonId = setPokemonId(inputMode);
   }
+
+  //pokeAPIより情報取得
   getPokemonInfo(pokemonId);
   getPokemonNameJa(pokemonId);
+
+  //今回のIDを保存
   pastPokemonId = pokemonId;
 }
 
 /*
 表示するポケモンIDを設定
 */
-function setPokemonId() {
-
+function setPokemonId(inputMode) {
   let pokemonId;
 
   //地域指定を取得
   let inputRegion = getInputRegion();
 
-  //ランダム指定を取得
-  let randomCheckbox = document.getElementById('selectRandomMode');
-  let eeveeCheckbox = document.getElementById('selecEeveeMode');
-
-  if (randomCheckbox.checked) {
+  //モードごとに分岐
+  if (inputMode === 'random') {
 
     //ランダムの場合、地域指定を考慮してIDを生成
     if (inputRegion === 'All') {
@@ -76,11 +83,13 @@ function setPokemonId() {
 
     pokemonId = getRandomIntInclusive(min, max);
     
-  } else if(eeveeCheckbox.checked) {
-    const eevee = [133, 134, 135, 136, 196, 197, 470, 471, 700]
+  } else if (inputMode === 'eevee') {
+    //イーブイモード
+    const eevee = [133, 134, 135, 136, 196, 197, 470, 471, 700];
     pokemonId = eevee[Math.floor(Math.random() * eevee.length)];
-  } else {
-    //ランダムでない場合は入力された指定IDを取得
+
+  } else if (inputMode === 'manual') {
+    //マニュアルモード
     const inputId = document.getElementById('inputPokemonId');
     pokemonId = inputId.value;
     //入力欄をクリア
@@ -104,12 +113,12 @@ function getRandomIntInclusive(min, max) {
 入力された地域を取得
 */
 function getInputRegion() {
-  const typeList = document.getElementsByName('region');
+  const regionList = document.getElementsByName('region');
   let inputRegion;
 
-  for (let i = 0; i < typeList.length; i++) {
-    if (typeList[i].checked) {
-      inputRegion = typeList[i].id;
+  for (let i = 0; i < regionList.length; i++) {
+    if (regionList[i].checked) {
+      inputRegion = regionList[i].id;
     }
   }
   console.log(`地域指定:${inputRegion}`);
@@ -124,7 +133,7 @@ function changeInputMode(){
   const inputPokemonId = document.getElementById("inputPokemonId");
   const inputMode = document.getElementById('inputMode').elements['inputMode'].value;
 
-  console.log(inputMode);
+  console.log(`モード変更:${inputMode}`);
 
   if (inputMode === 'random') {
     inputPokemonId.disabled = true;
